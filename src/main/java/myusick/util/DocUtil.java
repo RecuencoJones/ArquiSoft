b49095655f42a4aa212ba6dc737ef7f23b2919da
RecuencoJones <david.recuencogadea@gmail.com>
2015-03-10 18:04:14 +0100

    Even more doc

--- src/main/java/myusick/util/DocUtil.java
+++ src/main/java/myusick/util/DocUtil.java
@@ -1,11 +1,24 @@
 package myusick.util;
 
 public class DocUtil {
-    
-    public static String docEndpoint(String httpVerb, String path, String responseType, String description){
-        return "\t"+httpVerb+"\t"+path+"\n" +
-                "\tResponse-Type: "+responseType+"\n" +
-                "\t\t+ "+description+"\n\n";
-        
+
+    /**
+     * Método para documentar los endpoints
+     * @param httpVerb verbo HTTP usado
+     * @param path el path del endpoint
+     * @param type tipo de documento de petición o respuesta
+     * @param description descripción del endpoint
+     * @return string formateado con la documentación
+     */
+    public static String docEndpoint(String httpVerb, String path, String type, String description){
+        if(httpVerb.equalsIgnoreCase("post")) {
+            return "\t" + httpVerb + "\t" + path + "\n" +
+                    "\tConsumes: " + type + "\n" +
+                    "\t\t+ " + description + "\n\n";
+        }else{
+            return "\t" + httpVerb + "\t" + path + "\n" +
+                    "\tResponse-Type: " + type + "\n" +
+                    "\t\t+ " + description + "\n\n";
+        }
     }
 }
